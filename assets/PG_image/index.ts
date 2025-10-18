5487c8357c6786ee661d7d1c626dc3f58e7bccf0
Jagadish Pixart <unplugstories2@gmail.com>
2025-10-18 15:20:13 +0530

    mapping chnages

--- assets/PG_image/index.ts
+++ assets/PG_image/index.ts
@@ -1,3 +1,3 @@
 // assets/PG_image/index.ts
 // ✅ This file just re-exports from subfolders
-export { PG_ART_AND_CRAFT_IMAGES } from './PG_ART_AND_CRAFT_IMAGES';
+export { PG_ART_AND_CRAFT_IMAGES, PG_RHYMES_AND_STORIES_IMAGES, PG_PATTERN_IMAGES, PG_EVS_IMAGES, PG_ENGLISH_IMAGES} from './PG_Mapping';
